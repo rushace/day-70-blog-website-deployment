ca7e83641263516272d050f3b706cf0bc0d0812f
Shafin Khan <shafinkhan77477@gmail.com>
2024-10-10 17:45:53 +0530

    Use environment variable and create Procfile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import os
 from datetime import date
 from functools import wraps
 
@@ -30,7 +30,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -56,7 +56,8 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -240,4 +241,4 @@ def contact():
     return render_template("contact.html", current_user=current_user)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=7000)
+    app.run(debug=False)
